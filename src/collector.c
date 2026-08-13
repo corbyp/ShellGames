c2062a4d8d03d43762de71c1525406bb9401105c
corby <corbinian.porth@gmx.de>
2026-08-13 02:44:43 +0200

    overhauled collector to work with entity code

--- src/collector.c
+++ src/collector.c
@@ -7,8 +7,8 @@
 #include <stdlib.h>
 
 static const uint8_t score_limit = 10;
-static Entity player = {0, 0, NONE, '@', true, true};
-static Entity coin = {0, 0, NONE, 'o', true, true};
+static Entity player = {0, 0, '@', NONE, PLAYER, true, true};
+static Entity coin = {0, 0, 'o', NONE, ITEM, true, true};
 static uint8_t score = 0;
 static double move_cooldown = 0.15;
 static double move_timer = 0.0;
@@ -43,20 +43,23 @@ void update(Game *game) {
 
   if (move_timer >= move_cooldown) {
     move_timer -= move_cooldown;
-    safe_move(&player);
+    switch (move(&player)) {
+    case ITEM:
+      move_coin((*game).rows, (*game).cols);
+      ++score;
+      break;
+    default:
+      break;
+    }
 
     if (score >= score_limit)
       stop();
-
-    if (collide(&player, &coin)) {
-      move_coin((*game).rows, (*game).cols);
-      ++score;
-    }
   }
 }
 
 void setup(Game *game) {
   (void)game;
+  move_coin((*game).rows, (*game).cols);
   add_entity(&player);
   add_entity(&coin);
 }
